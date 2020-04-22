c98cea26872248b7ca2da5b74567da7e82342cf0
Magan Jain <Magan.Jain@walmartlabs.com>
2020-04-22 13:53:54 +0530

    AddTocart

--- analyticslibrary/src/main/java/com/walmart/analyticslibrary/LogUtils.java
+++ analyticslibrary/src/main/java/com/walmart/analyticslibrary/LogUtils.java
@@ -1,8 +1,10 @@
 package com.walmart.analyticslibrary;
 
 import android.content.Context;
+import android.os.Bundle;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 public class LogUtils  {
@@ -30,8 +32,14 @@ public class LogUtils  {
         return FirebaseInstanceId.getInstance();
     }
 
-
-
-
-
+    public void sendAddToCart() {
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "12345");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Nougat");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "Image");
+        bundle.putString(FirebaseAnalytics.Param.CURRENCY, "THB");
+        bundle.putString(FirebaseAnalytics.Param.TRANSACTION_ID, "111");
+        bundle.putString(FirebaseAnalytics.Param.VALUE, "300");
+        FirebaseAnalytics.getInstance(this.context).logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+    }
 }
