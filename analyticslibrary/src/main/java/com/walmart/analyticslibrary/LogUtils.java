bf670a1c3e883d5294ee007d866d6d0732a0d027
Magan Jain <Magan.Jain@walmartlabs.com>
2020-04-22 13:14:38 +0530

    Firebase

--- analyticslibrary/src/main/java/com/walmart/analyticslibrary/LogUtils.java
+++ analyticslibrary/src/main/java/com/walmart/analyticslibrary/LogUtils.java
@@ -1,8 +1,14 @@
 package com.walmart.analyticslibrary;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+
 public class LogUtils {
 
     public String getData() {
         return "ReaCHING HERE";
+    }
+
+    public FirebaseInstanceId getFirebaseInstanceId() {
+        return FirebaseInstanceId.getInstance();
     }
 }
