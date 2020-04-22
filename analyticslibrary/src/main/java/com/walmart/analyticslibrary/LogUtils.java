ab45bd2707aaabf38eef4aaefe6f6f60caf410c9
Magan Jain <Magan.Jain@walmartlabs.com>
2020-04-22 13:34:31 +0530

    Analytics events

--- analyticslibrary/src/main/java/com/walmart/analyticslibrary/LogUtils.java
+++ analyticslibrary/src/main/java/com/walmart/analyticslibrary/LogUtils.java
@@ -1,8 +1,26 @@
 package com.walmart.analyticslibrary;
 
+import android.content.Context;
+
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.iid.FirebaseInstanceId;
 
-public class LogUtils {
+public class LogUtils  {
+
+    Context context;
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void InitlizaeAnalyticsLibrary(Context context) {
+            this.context = context;
+        FirebaseApp.initializeApp(context);
+    }
 
     public String getData() {
         return "ReaCHING HERE";
@@ -11,4 +29,9 @@ public class LogUtils {
     public FirebaseInstanceId getFirebaseInstanceId() {
         return FirebaseInstanceId.getInstance();
     }
+
+
+
+
+
 }
